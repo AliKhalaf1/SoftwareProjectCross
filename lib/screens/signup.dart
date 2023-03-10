300ad6c2c171eec7b8ea32687120b20296717359
A7med-Amin <aahmedameen2000@gmail.com>
2023-03-10 23:59:51 +0200

    partial signUp

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -14,12 +14,46 @@ class Signup extends StatelessWidget {
           backgroundColor: Colors.white38,
           foregroundColor: Color.fromRGBO(0, 0, 0, 0.7),
           elevation: 0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text('Let\'s get started',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // fisrt column of page
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const <Widget>[
+                // child one
+                Text('Let\'s get started',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
+                SizedBox(height: 15,),
+                // child two
+                SizedBox(
+                  width: 250,
+                  child: Text(
+                    'Sign up or log in in to see what\'s happening near you',
+                    style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 0.8),
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+
+            //second column of page
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const <Widget>[
+                
+              ],
+
+            )
+          ],
+        ),
       ),
     );
   }
