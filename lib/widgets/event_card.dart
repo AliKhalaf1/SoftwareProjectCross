30bc265fd387e30c1abd637ed77e9971574278a0
A7med-Amin <aahmedameen2000@gmail.com>
2023-03-30 15:51:55 +0200

    edit share icon to be button in event card

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -9,15 +9,15 @@ import 'package:intl/intl.dart';
 import '../screens/sign_up/sign_up_or_log_in.dart';
 
 /// {@category Widgets}
-/// 
-///   It is StatefulWidget as its content changes depending on actions taken inside screen use this widget. 
-/// 
+///
+///   It is StatefulWidget as its content changes depending on actions taken inside screen use this widget.
+///
 ///   It consists of 2 widgets Inkwell and favIcon surrounded by stack.
-/// 
+///
 ///   If user can not add to favourite unless user logged-in or he will be navigated to login screen
-/// 
+///
 ///   Date format used here is
-///  
+///
 ///<strong>'EEE, MMM d • hh:mmaaa ' </strong>
 ///
 class EventCard extends StatefulWidget {
@@ -54,6 +54,10 @@ class _EventCardState extends State<EventCard> {
       });
     }
 
+    void share() {
+      return;
+    }
+
     return Stack(
       fit: StackFit.loose,
       children: [
@@ -83,7 +87,7 @@ class _EventCardState extends State<EventCard> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [                     
+                    children: [
                       Text(
                           '${DateFormat('EEE, MMM d • hh:mmaaa ').format(widget.event.date)} EET',
                           style: TextStyle(
@@ -103,37 +107,24 @@ class _EventCardState extends State<EventCard> {
                             : 'Offline',
                         style: const TextStyle(color: Colors.grey),
                       ),
-                      FittedBox(
-                        // width: 190,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Icon(
-                              key: Key("person"),
-                              Icons.person_outline_outlined,
-                              color: Color.fromRGBO(0, 0, 0, 0.7),
-                            ),
-                            widget.event.creatorFollowers < 10000
-                                ? Text(
-                                    '${widget.event.creatorFollowers} creator followers',
-                                    style: const TextStyle(
-                                        color: Color.fromRGBO(0, 0, 0, 0.7),
-                                        fontSize: 10,
-                                        fontWeight: FontWeight.w500))
-                                : Text(
-                                    '${widget.event.creatorFollowers} creator follow...',
-                                    style: const TextStyle(
-                                        color: Color.fromRGBO(0, 0, 0, 0.7),
-                                        fontSize: 10,
-                                        fontWeight: FontWeight.w500)),
-                            const Icon(
-                              key: Key("share"),
-                              Icons.share,
-                              size: 20,
-                              color: Color.fromRGBO(0, 0, 0, 0.7),
-                            ),
-                          ],
-                        ),
+                      Row(
+                        children: [
+                          const Icon(
+                            key: Key("person"),
+                            Icons.person_outline_outlined,
+                            color: Color.fromRGBO(0, 0, 0, 0.7),
+                          ),
+                          SizedBox(
+                            width: 95,
+                            child: Text(
+                                '${widget.event.creatorFollowers} creator followers',
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 0.7),
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500)),
+                          )
+                        ],
                       )
                     ],
                   ),
@@ -143,20 +134,34 @@ class _EventCardState extends State<EventCard> {
           ),
         ),
         Positioned(
-          bottom: 2,
-          right: 10,
-          child: IconButton(
-            onPressed: () => toggleFav(context),
-            icon: Icon(
-              !isFavourite()
-                  ? Icons.favorite_border_rounded
-                  : Icons.favorite_sharp,
-              color: !isFavourite()
-                  ? const Color.fromRGBO(0, 0, 0, 0.7)
-                  : const Color.fromARGB(255, 209, 65, 12),
-            ),
-          ),
-        ),
+            bottom: 2,
+            right: 10,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: share,
+                  icon: const Icon(
+                    key: Key("share"),
+                    Icons.share,
+                    size: 20,
+                    color: Color.fromRGBO(0, 0, 0, 0.7),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () => toggleFav(context),
+                  icon: Icon(
+                    key: const Key("fav"),
+                    !isFavourite()
+                        ? Icons.favorite_border_rounded
+                        : Icons.favorite_sharp,
+                    color: !isFavourite()
+                        ? const Color.fromRGBO(0, 0, 0, 0.7)
+                        : const Color.fromARGB(255, 209, 65, 12),
+                  ),
+                ),
+              ],
+            )),
       ],
     );
   }
