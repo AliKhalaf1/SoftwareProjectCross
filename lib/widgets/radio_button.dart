ecc96020eea773f92c0a204a5d03a1377f7666b9
A7med-Amin <aahmedameen2000@gmail.com>
2023-04-05 01:46:31 +0200

    Add radio button widget

---
+++ lib/widgets/radio_button.dart
@@ -0,0 +1,17 @@
+library RadioButton;
+
+import 'package:flutter/material.dart';
+
+class RadioButton extends StatefulWidget {
+  const RadioButton({super.key});
+
+  @override
+  State<RadioButton> createState() => _RadioButtonState();
+}
+
+class _RadioButtonState extends State<RadioButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
