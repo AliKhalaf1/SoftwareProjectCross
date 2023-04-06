0875414f832cbb28ee52606a969966ed4dee315a
A7med-Amin <aahmedameen2000@gmail.com>
2023-04-06 07:11:36 +0200

    Add Pick Date screen

--- lib/screens/filter_type_select.dart
+++ lib/screens/filter_type_select.dart
@@ -9,11 +9,38 @@ class FilterType extends StatelessWidget {
   final List<String> selections;
   const FilterType(this.title, this.selections, {super.key});
 
+  /// Show calendar in pop up dialog for selecting date range for calendar event.
+  void showDatePicker(BuildContext ctx) {
+    showDateRangePicker(
+      context: ctx,
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(const Duration(days: 365)),
+      builder: (context,child) {
+    return Theme(
+      data: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(backgroundColor: Color.fromARGB(255, 215, 67, 21)),
+        primaryColor: Colors.deepOrange[800],
+        badgeTheme: const BadgeThemeData(textColor: Colors.black),
+        buttonTheme:
+            const ButtonThemeData(textTheme: ButtonTextTheme.primary), colorScheme: const ColorScheme.light(primary: Color.fromARGB(255, 215, 67, 21), surface: Color.fromARGB(255, 215, 67, 21)).copyWith(secondary: Colors.deepOrange[800]),
+      ),
+      child: child!,
+    );
+  },
+
+    );
+  }
+
   /* Method to determine selection and navigate back to filters screen*/
   void selectFilteration(BuildContext ctx, int ind) {
-    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return FilterScreen([]);
-    }));
+    // Pick a date handler
+    if (selections.length == 7 && ind == 6) {
+      showDatePicker(ctx);
+    } else {
+      // Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      //   return FilterScreen([]);
+      // }));
+    }
   }
 
   @override
@@ -51,18 +78,18 @@ class FilterType extends StatelessWidget {
                 child: Padding(
                   padding:
                       const EdgeInsets.only(left: 14.0, bottom: 20, top: 20),
-                  child: 
-                  // Text(
-                  //       selections[index],
-                  //       style: const TextStyle(
-                  //           fontSize: 22,
-                  //           height: 0.9,
-                  //           letterSpacing: 1.3,
-                  //           fontFamily: 'Neue Plak Extended',
-                  //           fontWeight: FontWeight.w600,
-                  //           color: Color.fromRGBO(17, 3, 59, 1)),
-                  //     ),
-                  Row(
+                  child:
+                      // Text(
+                      //       selections[index],
+                      //       style: const TextStyle(
+                      //           fontSize: 22,
+                      //           height: 0.9,
+                      //           letterSpacing: 1.3,
+                      //           fontFamily: 'Neue Plak Extended',
+                      //           fontWeight: FontWeight.w600,
+                      //           color: Color.fromRGBO(17, 3, 59, 1)),
+                      //     ),
+                      Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
