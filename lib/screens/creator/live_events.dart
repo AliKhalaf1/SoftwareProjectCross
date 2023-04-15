2f237f58b32923f04f925891b2602b003226f696
Megs17 <ahmedfec2000@gmail.com>
2023-04-15 18:11:54 +0200

    LiveEventsCard Added

--- lib/screens/creator/live_events.dart
+++ lib/screens/creator/live_events.dart
@@ -1,18 +1,35 @@
 import 'package:Eventbrite/widgets/backgroud.dart';
 import 'package:flutter/material.dart';
 
+import '../../widgets/live_card.dart';
+
 class LiveEvents extends StatelessWidget {
   const LiveEvents({super.key});
   static const route = '/Liveevents';
+
   @override
   Widget build(BuildContext context) {
+    bool tester = true;
     return Scaffold(
-      body: Background("assets/images/live_events.jfif"),
+      body: tester
+          ? InkWell(
+              onTap: () {},
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  LiveCard('2023-05-25 20:00:00', 'event 1', 10, 5, 100),
+                  LiveCard('2023-05-25 20:00:00', 'event 1', 10, 0, 20),
+                ],
+              ),
+            )
+          : Background("assets/images/live_events.jfif"),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
-        child: Icon(Icons.add),
         backgroundColor: Theme.of(context).primaryColor,
+        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/widgets/live_card.dart
@@ -0,0 +1,68 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+
+class LiveCard extends StatelessWidget {
+  final String liveDescription;
+  final String dateString;
+  final int ticketsNumber;
+  final int ticketsTaken;
+  final int ticketPrice;
+  LiveCard(
+    this.dateString,
+    this.liveDescription,
+    this.ticketsNumber,
+    this.ticketsTaken,
+    this.ticketPrice,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime dateTime = DateTime.parse(dateString);
+    String formattedDate = DateFormat('EEE, MMM d, h:mm a')
+        .format(dateTime); // format the DateTime object
+    double percentageTickets = ticketsTaken / ticketsNumber;
+
+    return ListTile(
+      leading: CircularPercentIndicator(
+        radius: 50,
+        lineWidth: 3,
+        percent: percentageTickets,
+        progressColor: Colors.deepPurple,
+        backgroundColor: Colors.grey.shade300,
+        circularStrokeCap: CircularStrokeCap.round,
+        center: FittedBox(
+          child: Text('${(percentageTickets * 100).toInt()}%'),
+        ),
+      ),
+      title: Text(
+        liveDescription,
+        style: TextStyle(
+          color: Theme.of(context).cardColor,
+        ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            formattedDate,
+            style: TextStyle(
+              color: Theme.of(context).cardColor,
+            ),
+          ),
+          Text(
+            '$ticketsTaken / $ticketsNumber',
+            style: TextStyle(
+              color: Theme.of(context).cardColor,
+            ),
+          ),
+        ],
+      ),
+      trailing: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Text('\$$ticketPrice'),
+      ),
+    );
+  }
+}
