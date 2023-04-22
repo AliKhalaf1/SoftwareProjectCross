b3b732c78d0f753de4fcd91a8d397b7809988a38
ahmed <76961547+ahmedsaad562000@users.noreply.github.com>
2023-04-22 16:02:30 +0200

    Merging Sign-In logic with APIs

--- lib/helper_functions/constants.dart
+++ lib/helper_functions/constants.dart
@@ -2,6 +2,6 @@
 /// nodoc
 
 class Constants {
-  static const host = 'http://13.49.127.105:8000';
+  static const host = 'http://174.138.101.143/api';
   static const imgUploadApiKey = '3bfd1f546459d54f37a8f6880343c080';
 }
