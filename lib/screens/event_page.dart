601dd868985f8aa20c239b3495e6be2d349c3555
A7med-Amin <aahmedameen2000@gmail.com>
2023-05-01 13:32:48 +0300

    update

--- lib/screens/event_page.dart
+++ lib/screens/event_page.dart
@@ -1,8 +1,13 @@
 library SingleEventScreen;
 
+import '../helper_functions/log_in.dart';
+import '../providers/events/event.dart';
 import '../providers/events/events.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../providers/events/fav_events.dart';
+import 'sign_up/sign_up_or_log_in.dart';
 
 /// {@category User}
 /// {@category Screens}
@@ -10,19 +15,52 @@ import 'package:provider/provider.dart';
 ///
 /// This Page is used to display a single event data.
 ///
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
   static const eventPageRoute = '/Event-Page';
 
   @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  @override
   Widget build(BuildContext context) {
+    //----------------------- Event provider ------------------------------
+
+    // final event = Provider.of<Event>(context, listen: false);
+    // final favsData = Provider.of<FavEvents>(context);
+
+    //----------------------- Event id ------------------------------------
     final eventId =
         ModalRoute.of(context)?.settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Events>(
       context,
       listen: false,
     ).findById(eventId);
+
+    //----------------------- Methods ------------------------------
+
+    // Future<void> toggleFav(BuildContext ctx) async {
+    //   //add to favourites list
+    //   bool isLogged = await checkLoggedUser();
+    //   setState(() {
+    //     if (isLogged) {
+    //       //Call toggleStatus function from event class
+    //       if (event.isFav) {
+    //         favsData.removeEventFromFav(event);
+    //       } else {
+    //         favsData.addEventToFav(event);
+    //       }
+    //     } else {
+    //       Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+    //         return const SignUpOrLogIn();
+    //       }));
+    //     }
+    //   });
+    // }
+
     return Scaffold(
       key: const Key('EventPage'),
       //Edit app-bar to be as application
@@ -30,6 +68,29 @@ class EventPage extends StatelessWidget {
         backgroundColor: Colors.white38,
         foregroundColor: const Color.fromRGBO(0, 0, 0, 0.7),
         elevation: 0,
+        actions: [
+          Positioned(
+              bottom: 2,
+              right: 10,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  // IconButton(
+                  //   key: const Key("AddToFavBtn"),
+                  //   onPressed: () => toggleFav(context),
+                  //   icon: Icon(
+                  //     key: const Key("favIcon"),
+                  //     !event.isFav
+                  //         ? Icons.favorite_border_rounded
+                  //         : Icons.favorite_sharp,
+                  //     color: !event.isFav
+                  //         ? const Color.fromRGBO(0, 0, 0, 0.7)
+                  //         : const Color.fromARGB(255, 209, 65, 12),
+                  //   ),
+                  // ),
+                ],
+              )),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
