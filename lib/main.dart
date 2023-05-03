890d2abd7965157ec9672cfaa8f214180ec0c59a
Megs17 <ahmedfec2000@gmail.com>
2023-05-03 19:30:57 +0300

    Event main form added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:Eventbrite/providers/categories/categories.dart';
 import 'package:Eventbrite/providers/events/fav_events.dart';
 import 'package:Eventbrite/providers/filters/tags.dart';
 import 'package:Eventbrite/providers/filters/temp_tags.dart';
-import 'package:Eventbrite/screens/creator/description_title.dart';
+import 'package:Eventbrite/screens/creator/description_event.dart';
 import 'package:Eventbrite/screens/creator/event_location.dart';
 import 'package:Eventbrite/screens/creator/event_start_end_date.dart';
 import 'package:Eventbrite/screens/creator/event_title.dart';
