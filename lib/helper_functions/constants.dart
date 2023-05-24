32547e5e80fc2e286c36b4f446ac0672b9857376
ahmed <76961547+ahmedsaad562000@users.noreply.github.com>
2023-05-24 05:03:31 +0300

    Switched to live version

--- lib/helper_functions/constants.dart
+++ lib/helper_functions/constants.dart
@@ -4,5 +4,5 @@
 class Constants {
   static const host = 'http://44.210.36.161:5000';
   static const imgUploadApiKey = '3bfd1f546459d54f37a8f6880343c080';
-  static const MockServer = true;
+  static const MockServer = false;
 }
